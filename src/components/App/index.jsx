323b4e6fccdeea8a6f986eb0178eceb63fe8793b
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-28 15:13:17 +0300

    Add basic setup

---
+++ src/components/App/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import connect from '../../connect';
+
+const App = () => (
+  <div>Here goes an app</div>
+);
+
+export default connect()(App);
---
+++ src/redux/slices/index.js
@@ -0,0 +1,19 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+export default combineReducers({
+
+});
+
+const actions = {
+
+};
+
+export { actions };
+
+const selectors = {
+
+};
+
+const getSelector = (type) => selectors[type];
+
+export { getSelector };
