47be142c738f992003548dd594d885d39d4f4f82
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-29 19:41:22 +0300

    Add "PrivateRoute" component for private user area

---
+++ src/ui/components/PrivateRoute/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getSelector } from '../../../redux/slices';
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const userAuthenticatingState = useSelector(getSelector('userAuthenticatingState'));
+
+  return (
+    <Route
+      {...rest} // eslint-disable-line
+      render={({ location }) => {
+        const isAuthenticated = userAuthenticatingState.isFinished();
+
+        return isAuthenticated
+          ? (children)
+          : (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: { from: location },
+              }}
+            />
+          );
+      }}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default PrivateRoute;
--- src/ui/components/Root/index.jsx
+++ src/ui/components/Root/index.jsx
@@ -1,37 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
 } from 'react-router-dom';
+import PrivateRoute from '../PrivateRoute';
 import Login from '../../pages/Login';
-import Profile from '../../pages/Profile';
-import { getSelector } from '../../../redux/slices';
+import Details from '../../pages/Details';
 
-const Root = () => {
-  const userAuthenticatingState = useSelector(getSelector('userAuthenticatingState'));
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-
-        <Route path="/profile">
-          {!userAuthenticatingState.isFinished() && (
-            <Redirect to="/" />
-          )}
-
-          {userAuthenticatingState.isFinished() && (
-            <Profile />
-          )}
-        </Route>
-      </Switch>
-    </Router>
-  );
-};
+      <PrivateRoute path="/details">
+        <Details />
+      </PrivateRoute>
+    </Switch>
+  </Router>
+);
 
 export default Root;
