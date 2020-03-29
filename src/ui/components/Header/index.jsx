f8cd1213e65597ff39a8ca2b2fb6a0f10753d2fd
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-29 21:46:40 +0300

    Update "Header" component

--- src/ui/components/Header/index.jsx
+++ src/ui/components/Header/index.jsx
@@ -3,9 +3,11 @@ import './index.css';
 import logo from '../../assets/images/logo.svg';
 
 const Header = () => (
-  <header className="Header d-flex align-items-center justify-content-center bg-primary">
-    <div className="Header__logo">
-      <img src={logo} alt="Fast banana" />
+  <header className="Header position-relative w-100">
+    <div className="Header__container position-fixed w-100 d-flex align-items-center justify-content-center bg-primary">
+      <div className="Header__logo">
+        <img src={logo} alt="Fast banana" />
+      </div>
     </div>
   </header>
 );
