d5645771bda7b3aedbb54efe5b8fe936fa3d0490
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-29 16:48:10 +0300

    Add "Details" page

---
+++ src/ui/pages/Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import DetailsForm from '../components/DetailsForm';
+
+const Details = () => (
+  <Layout>
+    <DetailsForm />
+  </Layout>
+);
+
+export default Details;
