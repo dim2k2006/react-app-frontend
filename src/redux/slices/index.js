6cd2077fdbe78e1130a6d02b230ec6d6dc61fb5e
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-03-29 16:48:25 +0300

    Update slices

--- src/redux/slices/index.js
+++ src/redux/slices/index.js
@@ -1,18 +1,21 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import userAuthenticatingState, { actions as userAuthenticatingStateActions, getUserAuthenticatingState } from './userAuthenticatingState';
+import userUpdatingState, { actions as userUpdatingStateActions, getUserUpdatingState } from './userUpdatingState';
 import user, { actions as userActions } from './user';
 
 import errorMessage, { actions as errorMessageActions, getErrorMessage } from './errorMessage';
 
 export default combineReducers({
   userAuthenticatingState,
+  userUpdatingState,
   user,
   errorMessage,
 });
 
 const actions = {
   ...userAuthenticatingStateActions,
+  ...userUpdatingStateActions,
   ...userActions,
   ...errorMessageActions,
 };
@@ -21,6 +24,7 @@ export { actions };
 
 const selectors = {
   userAuthenticatingState: getUserAuthenticatingState,
+  userUpdatingState: getUserUpdatingState,
   errorMessage: getErrorMessage,
 };
 
